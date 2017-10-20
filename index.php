424675b2a5143aa0274ad108790e92fba0402800
maxgl73 <32264643+maxgl73@users.noreply.github.com>
2017-10-20 13:36:01 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  get_header();
+ ?>
+ <section>
+   <h3>Section centrale</h3>
+   <h3>Sans titre<h3>
+ </section>
+ <!--The loop-->
+ <?php if(have_posts()):while(have_posts()):
+   the_post(); ?>
+   <h3><?php the_title(); ?></h3>
+   <p><?php the_content(); ?></p>
+   <small>Publié le <?php the_time('j F Y') ?> à <?php the_time($d) ?></small>
+ <?php endwhile;endif; ?>
+ <?php get_sidebar(); ?>
+<?php
+  get_footer();
+ ?>
